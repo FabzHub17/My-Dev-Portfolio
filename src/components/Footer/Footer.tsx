cc54acc2d28e9cb7190fca413c5ea9f95b9c864f
fabio vitharana <fabio17.vitharana@gmail.com>
2022-09-26 12:38:54 +0200

    footer added

---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,13 @@
+import './_footer.scss';
+
+function Footer(){
+    return(
+        <>
+        <section className="footer">
+            <p className="paragraph">Designed and Built by Fabio Vitharana.</p>
+        </section>
+        </>
+    )
+}
+
+export default Footer;
