e8ab822a72fce89611905b6ce7350f6402c94efa
fabio vitharana <fabio17.vitharana@gmail.com>
2022-10-04 11:39:00 +0200

    contact me section animation added

--- src/components/Contact/Contact.tsx
+++ src/components/Contact/Contact.tsx
@@ -1,15 +1,20 @@
 import "./_contact.scss";
 
 function Contact() {
+
+  const sendEmail = () =>{
+    window.location.href='mailto:fabio17.vitharana@gmail.com?';
+
+  }
   return (
     <>
       <section className="contact">
         <h1 className="heading heading-primary">Hire Me!</h1>
         <p className="paragraph">
-          Let's work together! I'm eager to get my hands dirty with code and bring about my best work 
-          to satisfy my clients.
+          Let's work together! I'm eager to get my hands dirty with code and
+          bring about my best work to satisfy my clients.
         </p>
-        <button className="btn btn-primary">Get In Touch</button>
+        <button className="btn btn-primary" onClick={sendEmail}>Get In Touch</button>
       </section>
     </>
   );
